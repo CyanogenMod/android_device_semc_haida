e503028778198946b2e86cb4496fcfa4a34a5fc9
freexperia <jerpelea@gmail.com>
2012-06-15 17:38:49 +0300

    haida: updates for 4.1.B.0.431

--- prebuilt/hw_config.sh
+++ prebuilt/hw_config.sh
@@ -2,8 +2,8 @@
 dev=/sys/devices/platform/simple_remote.0
 echo 0,201,1600 > $dev/accessory_min_vals  # default = 0,200,1600
 echo 200,1599  > $dev/accessory_max_vals  # default = 200,1600
-echo 0,100,280,500,700 > $dev/button_min_vals  # default = 0,100,300,600,700
-echo 99,199,399,699,5000  > $dev/button_max_vals  # default = 99,199,399,699,5000
+echo 0,100,280,500,770 > $dev/button_min_vals  # default = 0,100,300,600,700
+echo 99,199,470,769,5000  > $dev/button_max_vals  # default = 99,199,399,699,5000
 echo 512   > $dev/btn_trig_period_freq  # Button Period Freq(Hz) default = 512
 echo 16  > $dev/btn_trig_period_time  # Button Period Time(cycle) default = 16
 echo 512   > $dev/btn_trig_hyst_freq    # Button Hysteresis Freq(Hz) default = 512
@@ -11,24 +11,12 @@ echo 16  > $dev/btn_trig_hyst_time   # Button Hysteresis Time(Cycle) default = 1
 echo 500 > $dev/btn_trig_level  # default = 500
 
 # Proximity sensor configuration
-dev=/sys/bus/i2c/devices/0-0054
-hwid=`cat /sys/class/hwid/hwid`
-case $hwid in
- 0x0a)
-  val_cycle=2
-  val_nburst=7
-  val_freq=3
-  val_threshold=15
-  val_filter=0
-  ;;
- *)
-  val_cycle=2
-  val_nburst=8
-  val_freq=2
-  val_threshold=15
-  val_filter=0
-  ;;
-esac
+dev=/sys/bus/i2c/devices/0-0054/
+val_cycle=0
+val_nburst=1
+val_freq=0
+val_threshold=4
+val_filter=0
 
 nv_param_loader 60240 prox_cal
 val_calibrated=$?
@@ -49,15 +37,21 @@ echo $val_filter > $dev/filter  # RFilter. Valid range is 0 - 3.
 
 # LMU AS3676 Configuration
 dev=/sys/devices/i2c-0/0-0040/leds
-echo 0,0,0,0,0,0,0 > $dev/lcd-backlight/als/curve  # ALS curve for group1
-echo 0,0,0,0,0,0,0 > $dev/button-backlight/als/curve  # ALS curve for group2
-echo 0,0,0,0,0,0,0 > $dev/keyboard-backlight/als/curve  # ALS curve for group2
-echo 0,0,0,0 > $dev/lcd-backlight/als/params  #[gain],[filter_up],[filter_down],[offset]
-echo 0 > $dev/lcd-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
-echo 0 > $dev/button-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
-echo 0 > $dev/keyboard-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
-echo 500 > $dev/button-backlight/max_current
-echo 5000 > $dev/keyboard-backlight/max_current
+echo 1,40,255,35,35,1,100 > $dev/lcd-backlight/als/curve  # ALS curve display BL [grp],[Y0],[Y3],[K1],[K2],[X1],[X2]
+echo 2,85,0,128,128,200,201 > $dev/button-backlight-rgb1/als/curve  # ALS curve key LED 1 [grp],[Y0],[Y3],[K1],[K2],[X1],[X2]
+echo 2,85,0,128,128,200,201 > $dev/button-backlight-rgb2/als/curve  # ALS curve key LED 2 [grp],[Y0],[Y3],[K1],[K2],[X1],[X2]
+echo 3,100,255,21,21,10,100 > $dev/red/als/curve  # ALS curve RGB(red) [grp],[Y0],[Y3],[K1],[K2],[X1],[X2]
+echo 3,100,255,21,21,10,100 > $dev/green/als/curve  # ALS curve RGB(green) [grp],[Y0],[Y3],[K1],[K2],[X1],[X2]
+echo 3,100,255,21,21,10,100 > $dev/blue/als/curve  # ALS curve RGB(blue) [grp],[Y0],[Y3],[K1],[K2],[X1],[X2]
+echo 1,2,2,0 > $dev/lcd-backlight/als/params  #[gain],[filter_up],[filter_down],[offset]
+echo 1 > $dev/lcd-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 1 > $dev/button-backlight-rgb1/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 1 > $dev/button-backlight-rgb2/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 1 > $dev/red/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 1 > $dev/green/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 1 > $dev/blue/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 900 > $dev/button-backlight-rgb1/max_current  #Max current in uA
+echo 900 > $dev/button-backlight-rgb2/max_current  #Max current in uA
 
 # TI BQ275xx firmware loader
 bq275xx_fwloader
