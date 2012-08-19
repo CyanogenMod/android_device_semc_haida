78e59f276699dd8255c43f5563bf9e54614c239b
freexperia <jerpelea@gmail.com>
2012-08-19 10:25:08 +0300

    haida: add device configs

--- haida.mk
+++ haida.mk
@@ -28,6 +28,17 @@ PRODUCT_COPY_FILES += \
     device/semc/haida/prebuilt/bootrec-device:root/sbin/bootrec-device \
     device/semc/haida/recovery.fstab:root/recovery.fstab
 
+# Device specific configs
+PRODUCT_COPY_FILES += \
+    device/semc/haida/config/cy8ctma300_touch.idc:system/usr/idc/cy8ctma300_touch.idc \
+    device/semc/haida/config/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
+    device/semc/haida/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
+    device/semc/haida/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
+    device/semc/haida/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    device/semc/haida/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/semc/haida/config/sensors.conf:system/etc/sensors.conf \
+    device/semc/haida/config/tiwlan.ini:system/etc/tiwlan.ini
+
 $(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
 
 #LCD DENSITY
