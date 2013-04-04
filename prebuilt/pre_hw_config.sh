515a44b5be884f9dbd7e3891c26e139a9b1f6c0c
Michael Bestas <mikeioannina@gmail.com>
2013-04-04 23:25:24 +0300

    haida: Remove obsolete ALS configuration
    Fix keyboard recovery indication
    Also set button_backlight current on pre_hw_config.sh to avoid too bright recovery indication.
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ie0e29c796b344704457fa9058b703d44ee910f8e

--- prebuilt/pre_hw_config.sh
+++ prebuilt/pre_hw_config.sh
@@ -1,6 +1,7 @@
 # LMU AS3676 Configuration before chargemon
 dev=/sys/devices/i2c-0/0-0040/leds
-echo 0 > $dev/lcd-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 900 > $dev/button-backlight-rgb1/max_current  #Max current in uA
+echo 900 > $dev/button-backlight-rgb2/max_current  #Max current in uA
 echo 20000 > $dev/lcd-backlight/max_current  #Max current in uA
 echo 5000 > $dev/red/max_current  #Max current in uA
 echo 5500 > $dev/green/max_current  #Max current in uA
