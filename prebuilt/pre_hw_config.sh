27b7556b2626e589051eb01bbcef5c060ce4c838
Michael Bestas <mikeioannina@gmail.com>
2013-04-05 19:22:17 +0300

    haida: Fix leds & backlight configuration on boot
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I181a700575ccf966fa7632ee0df852a6326ac752

--- prebuilt/pre_hw_config.sh
+++
@@ -1,9 +0,0 @@
-# LMU AS3676 Configuration before chargemon
-dev=/sys/devices/i2c-0/0-0040/leds
-echo 900 > $dev/button-backlight-rgb1/max_current  #Max current in uA
-echo 900 > $dev/button-backlight-rgb2/max_current  #Max current in uA
-echo 20000 > $dev/lcd-backlight/max_current  #Max current in uA
-echo 5000 > $dev/red/max_current  #Max current in uA
-echo 5500 > $dev/green/max_current  #Max current in uA
-echo 5000 > $dev/blue/max_current  #Max current in uA
-echo 2 > $dev/lcd-backlight/mode
