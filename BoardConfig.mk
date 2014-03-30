7042311d5f34849a2a3f1883401b223116002536
Michael Bestas <mikeioannina@gmail.com>
2014-03-30 22:34:23 +0300

    haida: Use new defconfig
    Change-Id: Icac9903a48588565ddda44a995c3ec475d8b5434

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ TARGET_SPECIFIC_HEADER_PATH += device/semc/haida/include
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/haida/bluedroid
 
 # Kernel
-TARGET_KERNEL_CONFIG := nAa_haida_defconfig
+TARGET_KERNEL_CONFIG := lx_haida_defconfig
 
 # Sensors
 SOMC_CFG_SENSORS_ACCEL_BMA150_INPUT := yes
